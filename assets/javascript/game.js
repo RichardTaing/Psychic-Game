982d447203af61556acc22acbf3b3b44c9202a5d
rst_979 <richard.taing@gmail.com>
2019-07-03 21:03:08 +1000

    Added link in html; Added description in games.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+/* BASICS FOR THE GAME:
+Computer makes a random letter choice
+User selects a letter
+Make sure user selection is a letter
+Capture the computer's choice and the user's guess
+Compare the choice and the guess
+Determine Wins & Losses
+Count the attempts made by user
+Record all guesses made by user
+When user wins, reset game, but not the stats
+When user runs out of guesses, reset game, but not the stats
+Display all game stats to the user */
+
+// Create an Array of options for both computer and user to choose from
